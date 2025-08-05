1fd5eaf971296f2103a7c26d2abac3e02192e2e5
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-08-05 17:57:55 +0100

    Handle errors.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const img = document.querySelector("img");
+const picture = document.querySelector("img");
 const searchBox = document.querySelector("input");
 searchBox.value = "Spider man";
 
 const fetchButton = document.querySelector("button");
-// window.onload = fetchGif(searchBox.value);
+// window.onload = fetchGif(searchBox.value,picture);
 fetchButton.addEventListener("click", () => {
   if (searchBox.value == "") return;
-  fetchGif(searchBox.value);
+  fetchGif(searchBox.value, picture);
 });
 
-function fetchGif(serchItem) {
+function fetchGif(serchItem, img) {
   const baseUrl = "https://api.giphy.com/v1/gifs/translate?";
   const params = new URLSearchParams({
     api_key: "ljAkFGcbOLgGmMiRpo50W7sZxNWYxlsv",
@@ -20,14 +20,27 @@ function fetchGif(serchItem) {
     mode: "cors",
     method: "GET",
   });
-  img.src = 'loading.webp';
+  img.src = "loading.webp";
+
   fetch(request)
     .then(function (response) {
-      return response.json();
+      console.log(response.status);
+      if (response.status != 200) {
+        throw new Error("re");
+      }
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`${response.status}`);
+      }
     })
     .then(function (response) {
-      console.log(response);
+      console.log(response.meta);
 
       img.src = response.data.images.original.url;
+    })
+    .catch(function (error) {
+      console.log(error);
+      img.src = "localhost-file-not-found.jpg";
     });
 }
