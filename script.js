eeecfd0bfd3e6cbfe066654b9b791a83f8975b2b
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-08-05 13:39:02 +0100

    Use Request Object in fetch method.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const img = document.querySelector("img");
-console.log(img);
-
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=ljAkFGcbOLgGmMiRpo50W7sZxNWYxlsv&s=flying sausage",
-  { mode: "cors" }
-).then(function (response) {
+const baseUrl = "https://api.giphy.com/v1/gifs/translate?";
+const params = new URLSearchParams();
+params.append("api_key", "ljAkFGcbOLgGmMiRpo50W7sZxNWYxlsv");
+params.append("s", "monkey");
+const request = new Request(`${baseUrl}${params}`, {
+  mode: "cors",
+  method: "GET",
+});
+fetch(request)
+  .then(function (response) {
     return response.json();
-  }).then(function (response) {
+  })
+  .then(function (response) {
     console.log(response);
-    
+
     img.src = response.data.images.original.url;
   });
