4217fed0b2376172dd59caa9d9612c2175b87442
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-08-05 15:41:19 +0100

    Implement gif fetching functionality.

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const img = document.querySelector("img");
-const baseUrl = "https://api.giphy.com/v1/gifs/translate?";
-const params = new URLSearchParams({api_key:"ljAkFGcbOLgGmMiRpo50W7sZxNWYxlsv",s:"monkey",weirdness:8});
-const request = new Request(`${baseUrl}${params}`, {
-  mode: "cors",
-  method: "GET",
-});
-// fetch(request)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (response) {
-//     console.log(response);
+const searchBox = document.querySelector("input");
+searchBox.value = "Spider man";
 
-//     img.src = response.data.images.original.url;
-//   });
+const fetchButton = document.querySelector("button");
+
+fetchButton.addEventListener("click", () => {
+  if (searchBox.value == "") return;
+  fetchGif(searchBox.value);
+});
+
+function fetchGif(serchItem) {
+  const baseUrl = "https://api.giphy.com/v1/gifs/translate?";
+  const params = new URLSearchParams({
+    api_key: "ljAkFGcbOLgGmMiRpo50W7sZxNWYxlsv",
+    s: serchItem,
+    weirdness: 8,
+  });
+  const request = new Request(`${baseUrl}${params}`, {
+    mode: "cors",
+    method: "GET",
+  });
+  fetch(request)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+
+      img.src = response.data.images.original.url;
+    });
+}
